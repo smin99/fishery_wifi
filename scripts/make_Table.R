c75cdaf4d708735d3d70efe465f8976caf38893d
Min Hwang <smhwang0715@gmail.com>
2018-12-02 14:55:41 -0800

    changed the arrange

--- scripts/make_Table.R
+++ scripts/make_Table.R
@@ -1,13 +1,17 @@
 # load libraries
 library(dplyr)
+library(kableExtra)
 
 allData <- read.csv("../data/data_filtered.csv")
 
 filter_data <- function(state){
   state_data <- allData %>% 
-    select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxCIRDown, MaxCIRUp) %>% 
-    filter(StateAbbr == state) %>% 
+    select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxAdDown, MaxAdUp, MaxCIRDown, MaxCIRUp) %>% 
+    filter(StateAbbr == "WA") %>% 
     group_by(ProviderName) %>% 
-    summarize(AvgMaxCIRDown=mean(MaxCIRDown), AvgMaxCIRUp=mean(MaxCIRUp)) %>% 
-    arrange(desc(AvgMaxCIRDown))
+    summarize(AvgMaxAdDown=mean(MaxAdDown), AvgMaxAdUp=mean(MaxAdUp), 
+              AvgMaxCIRDown=mean(MaxCIRDown), AvgMaxCIRUp=mean(MaxCIRUp)) %>% 
+    filter(AvgMaxCIRDown != 0 || AvgMaxAdDown != 0 || AvgMaxCIRUp != 0 || AvgMaxAdUp != 0)
+    arrange(desc(AvgMaxAdDown))
+  
 }
