0e079c62eaf4246af9fdec65cc1dc34bf7589bfc
Min Hwang <smhwang0715@gmail.com>
2018-12-02 14:56:04 -0800

    changed the arrange

--- scripts/make_Table.R
+++ scripts/make_Table.R
@@ -7,11 +7,11 @@ allData <- read.csv("../data/data_filtered.csv")
 filter_data <- function(state){
   state_data <- allData %>% 
     select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxAdDown, MaxAdUp, MaxCIRDown, MaxCIRUp) %>% 
-    filter(StateAbbr == "WA") %>% 
+    filter(StateAbbr == state) %>% 
     group_by(ProviderName) %>% 
     summarize(AvgMaxAdDown=mean(MaxAdDown), AvgMaxAdUp=mean(MaxAdUp), 
               AvgMaxCIRDown=mean(MaxCIRDown), AvgMaxCIRUp=mean(MaxCIRUp)) %>% 
-    filter(AvgMaxCIRDown != 0 || AvgMaxAdDown != 0 || AvgMaxCIRUp != 0 || AvgMaxAdUp != 0)
+    filter(AvgMaxCIRDown != 0 || AvgMaxAdDown != 0 || AvgMaxCIRUp != 0 || AvgMaxAdUp != 0) %>% 
     arrange(desc(AvgMaxAdDown))
   
 }
