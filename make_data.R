412ad492eda73a2713446443af7507ab9cbaa5d9
Howard Xiao <thx@uw.edu>
2018-12-01 16:03:20 -0800

    Push data generation script

---
+++ make_data.R
@@ -0,0 +1,23 @@
+# Follow this document to replicate the process of generating the datause for use in this project.
+library(data.table)
+library(dplyr)
+
+# From the FCC dataset website, export the data as a CSV file and read it in.
+# http://transition.fcc.gov/form477/BroadbandData/Fixed/Jun17/Version%201/US-Fixed-without-Satellite-Jun2017.zip
+#full_data <- fread("fbd_us_without_satellite_jun2017_v1.csv",
+full_data <- fread("fcc_small.csv",
+                   stringsAsFactors = FALSE) %>%
+  filter(StateAbbr != "PR" &
+         StateAbbr != "AK" &
+         StateAbbr != "HI" &
+         StateAbbr != "GU" &
+         StateAbbr != "AS" &
+         StateAbbr != "MP" &
+         StateAbbr != "VI")
+
+# For the UI, we also find all the states.
+conus_states <- unique(full_data$StateAbbr)
+
+# Write data.
+write.csv(full_data, "data_filtered.csv", row.names = FALSE)
+write.csv(conus_states, "states_filtered.csv", row.names = FALSE)
