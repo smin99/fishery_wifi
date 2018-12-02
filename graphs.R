4d4105c5ebe4079114b56ae3fd803131f932c718
Howard Xiao <thx@uw.edu>
2018-12-01 17:35:08 -0800

    Separate graphs into file

---
+++ graphs.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+make_pie_chart <- function(state, FCC_data) {
+  filtered_FCC <- FCC_data %>% filter(StateAbbr == state) %>% 
+    select(MaxAdDown)
+  range_1_less<- filtered_FCC %>% filter(MaxAdDown <= 1) %>% nrow()
+  range_1_10 <- filtered_FCC %>% filter(MaxAdDown > 1 & MaxAdDown < 10) %>% nrow()
+  range_10_100 <- filtered_FCC %>% filter(MaxAdDown >= 10 & MaxAdDown < 100) %>% nrow()
+  range_100_1000 <- filtered_FCC %>% filter(MaxAdDown >= 100 & MaxAdDown < 1000) %>% nrow()
+  range_1000_more <- filtered_FCC %>% filter(MaxAdDown >= 1000) %>% nrow()
+  range_name <- c("<1 mbps", "1-10 mbps", "10-100 mbps", "100-1000 mbps", "1000+ mbps")
+  range_value <- c(range_1_less,  range_1_10, range_10_100, range_100_1000, range_1000_more)
+  range_data <- data.frame(range_name, range_value)
+  range_data <- range_data %>% rename(range = range_name) %>% 
+    rename(value = range_value)
+  
+  ggplot(range_data, aes(x="", y=value, fill=range))+
+    geom_bar(width = 2, stat = "identity", color = "black") + 
+    coord_polar("y", start=0) + 
+    ggtitle(paste0("Max Advertised Download Speeds in ", state)) +
+    scale_fill_brewer(palette = "Set1") +
+    theme_void() +
+    theme(plot.title = element_text(size=22))
+}
+
