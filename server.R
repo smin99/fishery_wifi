e4f833ff53a52af7b213009a1035ce17e10da0e2
ktw0222 <kevstar1@uw.edu>
2018-12-01 18:38:36 -0800

    add heat map

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(data.table)
 library(mapdata)
+library(dplyr)
 source("graphs.R")
 source("analysis_text.R")
 
@@ -11,12 +12,20 @@ shinyServer(function(input, output) {
     make_pie_chart(input$selected_state, FCC_data)
   })
   output$nationalPlot <- renderPlot({
-    filtered_data <- FCC_data %>% select(StateAbbr, MaxAdDown)
-    state_averages
+    filtered_data <- FCC_data %>% select(StateName, MaxAdDown)
     states <- map_data("state")
-    states <- states %>% mutate(region = state.abb[which(state.name == region)])
+    states$median <- NA
+    state_median <- filtered_data %>% group_by(StateName) %>% summarize(median = mean(MaxAdDown))
+    for (i in 1:nrow(states)){
+      state_name <- states[i, "region"]
+      result <- filter(state_median, StateName == state_name)
+      if(nrow(result) != 0) {
+        states$median[i] = result$median
+      }
+    }
+
     ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group), color = "white") + 
+      geom_polygon(aes(x = long, y = lat, fill = median, group = group), color = "white") + 
       coord_fixed(1.3) +
       guides(fill=FALSE) + theme_void()
   })
