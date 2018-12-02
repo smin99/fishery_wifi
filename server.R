af543e5f7eef509b2305c5e3afda34d25e27619e
ktw0222 <kevstar1@uw.edu>
2018-12-01 17:29:26 -0800

    add pie chart

--- server.R
+++ server.R
@@ -1,18 +1,21 @@
 library(shiny)
 library(data.table)
+library(ggplot2)
+library(openintro)
 
 FCC_data <- fread("data_filtered.csv", stringsAsFactors = FALSE)
 shinyServer(function(input, output) {
   # graph the state data pie chart
   output$statePlot <- renderPlot({
-    filtered_FCC <- FCC_data %>% filter(StateAbbr == toString(input$selected_state)) %>% 
+    filtered_FCC <- FCC_data %>% filter(StateAbbr == as.character(input$selected_state)) %>% 
       select(MaxAdDown)
-    range_1_10 <- filtered_FCC %>% filter(MaxAdDown >= 1 & MaxAdDown < 10) %>% nrow()
+    range_1_less<- filtered_FCC %>% filter(MaxAdDown <= 1) %>% nrow()
+    range_1_10 <- filtered_FCC %>% filter(MaxAdDown > 1 & MaxAdDown < 10) %>% nrow()
     range_10_100 <- filtered_FCC %>% filter(MaxAdDown >= 10 & MaxAdDown < 100) %>% nrow()
     range_100_1000 <- filtered_FCC %>% filter(MaxAdDown >= 100 & MaxAdDown < 1000) %>% nrow()
     range_1000_more <- filtered_FCC %>% filter(MaxAdDown >= 1000) %>% nrow()
-    range_name <- c("1-10 mbps", "10-100 mbps", "100-1000 mbps", "1000+ mbps")
-    range_value <- c(range_1_10,  range_10_100,  range_100_1000,  range_1000_more)
+    range_name <- c("<1 mbps", "1-10 mbps", "10-100 mbps", "100-1000 mbps", "1000+ mbps")
+    range_value <- c(range_1_less,  range_1_10, range_10_100, range_100_1000, range_1000_more)
     range_data <- data.frame(range_name, range_value)
     range_data <- range_data %>% rename(range = range_name) %>% 
       rename(value = range_value)
@@ -20,7 +23,9 @@ shinyServer(function(input, output) {
     ggplot(range_data, aes(x="", y=value, fill=range))+
       geom_bar(width = 2, stat = "identity", color = "black") + 
       coord_polar("y", start=0) + 
-      ggtitle("Total Sightings of each UFO shapes") +
+      ggtitle(paste0("Max Advertised Download Speeds in ", input$selected_state)) +
+      scale_fill_brewer(palette = "Set1") +
+      theme_void() +
       theme(plot.title = element_text(size=22))
     
   })
