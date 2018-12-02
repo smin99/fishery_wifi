8f1378645ad3cdfffd2c1a454ccd911316aa7dc8
Min Hwang <smhwang0715@gmail.com>
2018-12-02 14:43:45 -0800

    table reflect state change

--- scripts/make_Table.R
+++ scripts/make_Table.R
@@ -6,7 +6,7 @@ allData <- read.csv("../data/data_filtered.csv")
 filter_data <- function(state){
   state_data <- allData %>% 
     select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxCIRDown, MaxCIRUp) %>% 
-    filter(StateAbbr == "WA") %>% 
+    filter(StateAbbr == state) %>% 
     group_by(ProviderName) %>% 
     summarize(AvgMaxCIRDown=mean(MaxCIRDown), AvgMaxCIRUp=mean(MaxCIRUp)) %>% 
     arrange(desc(AvgMaxCIRDown))
