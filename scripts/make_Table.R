0fba18140160e73be8f7a64de337e7f51f48f7ad
wubcrow <21995564+wubcrow@users.noreply.github.com>
2018-12-03 11:24:06 -0800

    Remove business speeds

--- scripts/make_Table.R
+++ scripts/make_Table.R
@@ -6,11 +6,10 @@ allData <- read.csv("../data/data_filtered.csv")
 
 filter_data <- function(state){
   state_data <- allData %>% 
-    select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxAdDown, MaxAdUp, MaxCIRDown, MaxCIRUp) %>% 
+    select(ProviderName, DBAName, HoldingCompanyName, HocoFinal, StateAbbr, MaxAdDown, MaxAdUp) %>% 
     filter(StateAbbr == state) %>% 
     group_by(ProviderName) %>% 
-    summarize(AvgMaxAdDown=mean(MaxAdDown), AvgMaxAdUp=mean(MaxAdUp), 
-              AvgMaxCIRDown=mean(MaxCIRDown), AvgMaxCIRUp=mean(MaxCIRUp)) %>% 
+    summarize(AvgMaxAdDown=mean(MaxAdDown), AvgMaxAdUp=mean(MaxAdUp)) %>% 
     filter(AvgMaxAdDown != 0) %>% 
     arrange(desc(AvgMaxAdDown)) %>% 
     knitr::kable(format="html", digits=2, caption=paste0("Table 1: Speed of each Provider in ", state)) %>% 
