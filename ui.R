d297abca1d15d4262eeeb615e9420137279d8501
ktw0222 <kevstar1@uw.edu>
2018-12-04 15:10:27 -0800

    fix typo

--- ui.R
+++ ui.R
@@ -36,11 +36,11 @@ shinyUI(dashboardPage(
               ),
               fluidRow(
                 box(plotOutput("nationalPlot"), width = 12, background = "teal"),
-                box(p("This plot shows a heatmap of advertised Internet speeds in the contiguous United States,
+                box(p("This plot shows a heat map of advertised Internet speeds in the contiguous United States,
                       separated by state and the averages of advertised internet speeds. As some states have
                       less Internet Service Providers, they may appear to have higher averages, as opposed to
                       data from larger states which may too be skewed by a large amount of rural dialup. 
-                      Supplement your knwoledge with the following charts and tables to help you make a better
+                      Supplement your knowledge with the following charts and tables to help you make a better
                       informed decision."), width=12, background = "black")
               )
               
